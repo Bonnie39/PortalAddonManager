c0ed2c68d0bc0342917fecfe8d29b5ec26980fde
BonnieDev <benwestphal39@hotmail.com>
2022-10-03 20:36:40 +1000

    Minor fixes
    Fixed disabled addons showing as enabled again when re-opening the software and attempting to disable them again resulting in an error

--- PortalAddonManager/PortalAddonManager.cs
+++ PortalAddonManager/PortalAddonManager.cs
@@ -1,10 +1,29 @@
 using System;
+using System.Linq;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Diagnostics;
 
 namespace PortalAddonManager
 {
+
+    public static class ConcatFunction
+    {
+        public static T[] Concatenate<T>(this T[] first, T[] second)
+        {
+            if (first == null)
+            {
+                return second;
+            }
+            if (second == null)
+            {
+                return first;
+            }
+
+            return first.Concat(second).ToArray();
+        }
+    }
+
     public partial class PortalAddonManager : Form
     {
         private string? steamPath;
@@ -13,7 +32,9 @@ namespace PortalAddonManager
         private static string? addonOrigin;
         private static string? addonDestination;
 
-        string[] addons;
+        private readonly string[] addons = Array.Empty<string>();
+        private readonly string[] disabledAddons = Array.Empty<string>();
+        private string[] fullAddonList = Array.Empty<string>();
 
         public PortalAddonManager()
         {
@@ -24,6 +45,7 @@ namespace PortalAddonManager
             if (portalAddonPath != null)
             {
                 addons = Directory.GetFiles(portalAddonPath, "*.vpk");
+                disabledAddons = Directory.GetFiles(portalAddonPath, "*.inactive");
                 LookForAddons();
             }
 
@@ -68,23 +90,56 @@ namespace PortalAddonManager
             checkedListBox1.DisplayMember = "name";
             checkedListBox1.ValueMember = "description";
 
-            // Add each .vpk addon file to the checkedboxlist and set it to active by default
-            for (int i = 0; i < addons.Length; i++)
-            {
-                checkedListBox1.Items.Insert(i, new Addon { Name = Path.GetFileName(addons[i]), Description = "test" });
-                checkedListBox1.SetItemChecked(i, true);
-            }
 
+            // Add each .vpk addon file to the checkedboxlist and set it to active by default if enabled and inactive by default if disabled
+            if (addons != null | disabledAddons != null)
+            {
+
+                fullAddonList = addons.Concatenate(disabledAddons);
+
+                for (int i = 0; i < fullAddonList.Length; i++)
+                {
+                    string temp = Path.GetFileName(fullAddonList[i]);
+                    string? temp2;
+
+                    if (temp.EndsWith(".inactive"))
+                    {
+                        temp2 = temp.Remove(temp.Length - 9, 9);
+                        checkedListBox1.Items.Insert(i, new Addon { Name = temp2, Description = "test" });
+                        checkedListBox1.SetItemChecked(i, false);
+                    }
+                    else
+                    {
+                        temp2 = temp;
+                        checkedListBox1.Items.Insert(i, new Addon { Name = temp2, Description = "test" });
+                        checkedListBox1.SetItemChecked(i, true);
+                    }
+
+                }
+
+            }
         }
 
         #endregion
 
+        #region FUNCTIONS_INITIALLY_GENERATED_BY_VISUAL_STUDIO
         private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
         {
             // If we uncheck, disable the addon
             if(checkedListBox1.GetItemChecked(e.Index) != false)
             {
-                addonOrigin = addons[e.Index].ToString();
+                if (fullAddonList != null)
+                {
+                    if (fullAddonList[e.Index].EndsWith(".inactive"))
+                    {
+                        addonOrigin = fullAddonList[e.Index].ToString().Remove(fullAddonList[e.Index].Length - 9, 9);
+                    }
+                    else
+                    {
+                        addonOrigin = fullAddonList[e.Index].ToString();
+                    }
+                }
+
                 addonDestination = addonOrigin + ".inactive";
 
                 if (addonOrigin != null && addonDestination != null)
@@ -100,7 +155,18 @@ namespace PortalAddonManager
             // If we check, enable the addon
             if(checkedListBox1.GetItemChecked(e.Index) == false)
             {
-                addonOrigin = addons[e.Index].ToString();
+                if(fullAddonList != null)
+                {
+                    if (fullAddonList[e.Index].EndsWith(".inactive"))
+                    {
+                        addonOrigin = fullAddonList[e.Index].ToString().Remove(fullAddonList[e.Index].Length - 9, 9);
+                    }
+                    else
+                    {
+                        addonOrigin = fullAddonList[e.Index].ToString();
+                    }
+                }
+
                 addonDestination = addonOrigin + ".inactive";
 
                 if (addonOrigin != null && addonDestination != null)
@@ -113,5 +179,6 @@ namespace PortalAddonManager
                 }
             }
         }
+        #endregion
     }
 }
