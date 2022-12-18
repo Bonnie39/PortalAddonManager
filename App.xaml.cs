c6bbcda3cdbe92ebb39e340db0e28db0025b8a8d
BonnieDev <benwestphal39@hotmail.com>
2022-12-18 17:07:19 +1000

    Migrated development to .NET MAUI Blazor from .NET WinForms

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace PortalAddonManager;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+
+	protected override Window CreateWindow(IActivationState activationState)
+	{
+		var window = base.CreateWindow(activationState);
+		if (window != null)
+		{
+			window.Title = "PortalAddonManager";
+		}
+
+		return window;
+	}
+}
---
+++ Scripts/Concat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalAddonManager.Scripts
+{
+    public static class Concat
+    {
+        public static T[] Concatenate<T>(this T[] first, T[] second)
+        {
+            if (first == null)
+            {
+                return second;
+            }
+            if (second == null)
+            {
+                return first;
+            }
+
+            return first.Concat(second).ToArray();
+        }
+    }
+}
